f21cee3141ec4bb06e4e23cf13c1d45fbffedb15
steve_tsao <steve_tsao@neithnet.com>
2021-02-25 15:34:40 +0800

    1st commit

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="text-gray-600">
+      <header class="mb-5">
+        <div class="bg-gray-700 p-4 text-white">Helo world.</div>
+      </header>
+      <main class="container mx-auto">
+        <div class="text-center text-5xl">我也為此而來！</div>
+        <hr class="my-5" />
+        <div class="flex justify-center">
+          <img
+            src="https://raw.githubusercontent.com/komavideo/LearnTailwindCSS/main/tailwindcss.png"
+            class="shadow-lg border my-5"
+          />
+        </div>
+      </main>
+      <footer class="mt-5">
+        <div class="bg-gray-300 p-4 text-black">你好，Tailwind CSS。</div>
+      </footer>
+    </div>
+  </div>
+</template>
