aa0231b156203830ff6b55a9f3035c5506596ef2
steve_tsao <steve_tsao@neithnet.com>
2021-02-26 16:16:08 +0800

    to p.6

--- pages/about.vue
+++ pages/about.vue
@@ -6,6 +6,24 @@
       </header>
       <main class="container mx-auto">
         <div class="text-center text-5xl">我也為此而來！</div>
+        <hr />
+        <div class="text-komabig">雪莉大字體</div>
+        <div class="my-5 bg-bootstrap_primary text-white">
+          你剛剛攻打我的村莊！？
+        </div>
+        <div class="my-5 bg-bootstrap_success text-white">
+          你剛剛攻打我的村莊！？
+        </div>
+        <div class="my-5 bg-bootstrap_danger text-white">
+          你剛剛攻打我的村莊！？
+        </div>
+        <!-- 循环各种字号 Start -->
+        <ul>
+          <li v-for="(fontsize, i) in list" :key="i">
+            <div :class="fontsize">Tailwind CSS</div>
+          </li>
+        </ul>
+        <!-- 循环各种字号 End -->
         <hr class="my-5" />
         <div class="flex justify-center">
           <img
@@ -20,3 +38,27 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        "text-xs",
+        "text-sm",
+        "text-base",
+        "text-lg",
+        "text-xl",
+        "text-2xl",
+        "text-3xl",
+        "text-4xl",
+        "text-5xl",
+        "text-6xl",
+        "text-7xl",
+        "text-8xl",
+        "text-9xl"
+      ]
+    };
+  }
+};
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      fontSize: { komabig: ["10rem", { lineHeight: "10rem" }] },
+      colors: {
+        bootstrap_primary: "#0d6efd",
+        bootstrap_secondary: "#6c757d",
+        bootstrap_success: "#198754",
+        bootstrap_danger: "#dc3545",
+        bootstrap_info: "#0dcaf0"
+      }
+    }
   },
   variants: {
     extend: {}
